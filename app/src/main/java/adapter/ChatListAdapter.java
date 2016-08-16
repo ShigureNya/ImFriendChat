7a3256369f0a280d06c258bfc7783637e49f4cbe
jimhao <806869597@qq.com>
2016-08-16 17:51:11 +0800

    2016-08-16 需要尝试是用三级缓存来加载网络图片,逻辑为cache/disk/network

--- app/src/main/java/adapter/ChatListAdapter.java
+++ app/src/main/java/adapter/ChatListAdapter.java
@@ -7,19 +7,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.target.Target;
+import com.hyphenate.EMCallBack;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMConversation;
 import com.hyphenate.chat.EMImageMessageBody;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.chat.EMTextMessageBody;
 
+import java.io.IOException;
 import java.util.List;
 
 import cc.jimblog.imfriendchat.R;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+import util.ImageDownload;
 import util.LogUtils;
 import view.CircleImageView;
 
@@ -38,6 +52,8 @@ public class ChatListAdapter extends BaseAdapter {
         this.mMessageList = list;
         mInflater = LayoutInflater.from(context);
     }
+    private static final int DEFAULT_WIDTH = 350 ;
+    private static final int DEFAULT_HEIGHT = 350 ;
     @Override
     public int getCount() {
         return mMessageList.size();
@@ -64,42 +80,49 @@ public class ChatListAdapter extends BaseAdapter {
             viewHolder.messageLeftText = (TextView) convertView.findViewById(R.id.chat_left_text);
             viewHolder.userLeftImg = (CircleImageView) convertView.findViewById(R.id.chat_left_img);
             viewHolder.tokenLeftLayout = (RelativeLayout) convertView.findViewById(R.id.chat_left_msg_layout);
+            viewHolder.messageLeftImage = (ImageView) convertView.findViewById(R.id.chat_left_text_img);
 
             viewHolder.messageRightText = (TextView) convertView.findViewById(R.id.chat_right_text);
             viewHolder.userRightImg = (CircleImageView) convertView.findViewById(R.id.chat_right_img);
             viewHolder.tokenRightLayout = (RelativeLayout) convertView.findViewById(R.id.chat_right_msg_layout);
+            viewHolder.messageRightImage = (ImageView) convertView.findViewById(R.id.chat_right_text_img);
 
             convertView.setTag(viewHolder);
         }else{
             viewHolder = (ViewHolder) convertView.getTag();
         }
+        //如果是接收到的消息
         if(emMessage.direct() == EMMessage.Direct.RECEIVE){
             viewHolder.tokenRightLayout.setVisibility(View.GONE);   //首先关闭右侧的视图
             viewHolder.tokenLeftLayout.setVisibility(View.VISIBLE); //并打开左侧视图
             //处理接收到的消息
             if(emMessage.getType() == EMMessage.Type.TXT){
+                viewHolder.messageLeftImage.setVisibility(View.GONE);
+                viewHolder.messageLeftText.setVisibility(View.VISIBLE);
                 EMTextMessageBody textBody = (EMTextMessageBody) emMessage.getBody();
                 String message = textBody.getMessage();
                 viewHolder.messageLeftText.setText(message);
             }else if(emMessage.getType() == EMMessage.Type.IMAGE){
+                viewHolder.messageLeftImage.setVisibility(View.VISIBLE);
+                viewHolder.messageLeftText.setVisibility(View.GONE);
                 EMImageMessageBody imageBody = (EMImageMessageBody) emMessage.getBody();
-                String url = imageBody.getThumbnailUrl(); //调用此方法SDK会自动下载图片到本地,此URL为本地存储的路径
-                //Bitmap downloadBitmap = BitmapFactory.decodeFile(url);
-                LogUtils.i("图片地址为:"+url);
+                netLoadChatImage(imageBody,viewHolder.messageLeftImage);
             }
-        }else{
+        }else{ //如果是自己发送的消息
             viewHolder.tokenRightLayout.setVisibility(View.VISIBLE);
             viewHolder.tokenLeftLayout.setVisibility(View.GONE);
             //处理接收到的消息
             if(emMessage.getType() == EMMessage.Type.TXT){
+                viewHolder.messageRightImage.setVisibility(View.GONE);
+                viewHolder.messageRightText.setVisibility(View.VISIBLE);
                 EMTextMessageBody textBody = (EMTextMessageBody) emMessage.getBody();
                 String message = textBody.getMessage();
                 viewHolder.messageRightText.setText(message);
             }else if(emMessage.getType() == EMMessage.Type.IMAGE){
+                viewHolder.messageRightImage.setVisibility(View.VISIBLE);
+                viewHolder.messageRightText.setVisibility(View.GONE);
                 EMImageMessageBody imageBody = (EMImageMessageBody) emMessage.getBody();
-                String url = imageBody.getThumbnailUrl(); //调用此方法SDK会自动下载图片到本地,此URL为本地存储的路径
-                //Bitmap downloadBitmap = BitmapFactory.decodeFile(url);
-                LogUtils.i("图片地址为:"+url);
+                netLoadChatImage(imageBody,viewHolder.messageRightImage);
             }
         }
 
@@ -109,10 +132,12 @@ public class ChatListAdapter extends BaseAdapter {
     class ViewHolder{
         public CircleImageView userLeftImg ;    //对方用户头像
         public TextView messageLeftText ;  //对方消息实体
+        public ImageView messageLeftImage ;     //对方消息图片实体
         public RelativeLayout tokenLeftLayout ;   //对方Layout 作显示和隐藏
 
         public CircleImageView userRightImg ;   //自己用户头像
         public TextView messageRightText ;  //自己消息实体
+        public ImageView messageRightImage ;    //自己消息图片实体
         public RelativeLayout tokenRightLayout ;  //自己Layout 作显示和隐藏
     }
     @Override
@@ -123,5 +148,76 @@ public class ChatListAdapter extends BaseAdapter {
     @Override
     public boolean isEnabled(int position) {
         return false;
+    }
+
+    /**
+     * 为了保证ListView流畅滚动,这里是用谷歌官方推荐的Glide图片加载库来实现
+     * update 因为发现是用EMImageMessageBody 无法加载图片的实际大小
+     * 决定用RxJava异步download图片,从Bitmap中得到准确的大小
+     *
+     * 这边的数据保存算法需要修改,需要增加三级缓存模型,即 cache → Storage → NetWork
+     * @param imageBody 图像实体
+     */
+    private void netLoadChatImage(EMImageMessageBody imageBody, final ImageView imageView){
+        final String url = imageBody.getThumbnailUrl(); //调用此方法SDK会自动下载图片到本地,此URL为本地存储的路径
+        //创建被监听者对象
+        Observable<Bitmap> observable = Observable.create(new Observable.OnSubscribe<Bitmap>() {
+            @Override
+            public void call(Subscriber<? super Bitmap> subscriber) {
+                try {
+                    Bitmap bitmap = ImageDownload.getBitmap(url);
+                    subscriber.onNext(bitmap);
+                    subscriber.onCompleted();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    subscriber.onError(new Throwable(e.toString()));
+                }
+            }
+        });
+        observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<Bitmap>() {
+                    @Override
+                    public void onCompleted() {
+                        LogUtils.i("图片下载成功");
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                        LogUtils.e("图片下载失败:"+throwable.toString());
+                    }
+
+                    @Override
+                    public void onNext(Bitmap bitmap) {
+                        int width = bitmap.getWidth() ;
+                        int height = bitmap.getHeight();
+                        LogUtils.d("Width:"+width+",height:"+height);
+                        int displayWidth = 0 ;
+                        int displayHeight = 0 ;
+                        if(width > height){
+                            float dpi = (float)width / height ;
+                            if(width > DEFAULT_WIDTH){
+                                displayWidth = DEFAULT_WIDTH;
+                                displayHeight = (int) (displayWidth / dpi);
+                            }else{
+                                displayWidth = width ;
+                                displayHeight = height ;
+                            }
+                        }else{
+                            float dpi = (float)height / width ;
+                            if(height > DEFAULT_HEIGHT){
+                                displayHeight = DEFAULT_HEIGHT ;
+                                displayWidth = (int) (displayHeight / dpi);
+                            }else{
+                                displayWidth = width ;
+                                displayHeight = height ;
+                            }
+                        }
+                        Glide.with(mContext)
+                                .load(url)
+                                .override(displayWidth,displayHeight)
+                                .centerCrop()
+                                .into(imageView);
+                    }
+                });
     }
 }
