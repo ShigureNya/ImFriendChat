d6c55f3ef8b1db3671f66b4d3248e7bf453ed754
Ran <806869597@qq.com>
2016-08-25 00:57:52 +0800

    2016-8-25 添加了对BmobSDK后端云的支持

--- app/src/main/java/cc/jimblog/imfriendchat/WelcomeActivity.java
+++ app/src/main/java/cc/jimblog/imfriendchat/WelcomeActivity.java
@@ -2,26 +2,28 @@ package cc.jimblog.imfriendchat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.hyphenate.chat.EMClient;
 
+import cn.bmob.v3.Bmob;
 import util.LogUtils;
 
 /**
  * Created by Kotori on 2016/8/5.
  */
 public class WelcomeActivity extends AppCompatActivity {
-
+    private static final String BMOB_KEY = "0d79f597ccb8897f2cbdb5493f5827b2";
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         //判断是否登陆过服务器
         boolean isLogin = EMClient.getInstance().isLoggedInBefore();
+        //初始化BmobSDK
+        Bmob.initialize(WelcomeActivity.this,BMOB_KEY);
         Log.i("Tag",isLogin+"");
         Intent intent = new Intent();
         //此处可以修改为延迟跳转
